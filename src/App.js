7fd451eb8c01310b2c11d9c417b15bf15c201c15
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-01-10 13:14:43 +0530

    added about section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import AboutUs from "./pages/AboutUs";
 function App() {
     return (
         <div className="App">
-            <h1> Capture Portfolio</h1>
+            <AboutUs />
         </div>
     );
 }
